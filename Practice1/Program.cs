d25628d2204f509f9ddbb8544b5ccf51cd9e44fb
dizare <bets4pro@mail.ru>
2023-02-01 10:31:33 +0500

    Добавьте файлы проекта.

---
+++ Practice1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Text;
+namespace Practice1
+{
+    class Program
+    {
+        static string ChangedStr(string str)
+        {
+            string newStr;
+            char[] mass = str.ToCharArray();
+            int startPos = 0;
+            
+            if (str.Length == 0)
+            {
+                return "Неверный ввод";
+            }
+            else
+            {
+                if (str.Length % 2 == 0)
+                {
+                    Array.Reverse(mass);
+                }
+            }
+            return new string(mass);
+
+        }
+        static void Main(string[] args)
+        {
+            string strInput;
+            Console.WriteLine("Введите строку");
+            strInput = Console.ReadLine();
+            Console.WriteLine();
+            strInput = ChangedStr(strInput);
+            Console.WriteLine(strInput);
+        }
+    }
+}
