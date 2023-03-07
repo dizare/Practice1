980b355dd383aab1b44376f668cbfe257f92e07e
dizare <bets4pro@mail.ru>
2023-03-07 23:58:33 +0500

    Быстрая сортировка (Quicksort) и Сортировка деревом (Tree sort)

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -14,77 +14,89 @@ namespace Practice1
             Console.WriteLine(strResult);
             NumOfReplays(strResult);
             SubString(strResult);
-        }
-        static string ChangedStr(string str)
-        {
-            char[] mass = str.ToCharArray();
-            if (str.Length % 2 == 0)
+
+            Console.WriteLine("Выберите метод сортировки обработанной строки: \nQ - Быстрая сортировка, T - Сортировка деревом");
+            string choice = Console.ReadLine().ToUpper();
+            if (choice == "Q")
             {
-                int mid = (mass.Length + 1) / 2;
-                char[] firstHalf = mass.Take(mid).ToArray();
-                char[] secondHalf = mass.Skip(mid).ToArray();
-                Array.Reverse(firstHalf);
-                Array.Reverse(secondHalf);
-                return new string(firstHalf) + new string(secondHalf);
+                Console.WriteLine("\nРезультат сортировки QuickSort: ");
+                Console.WriteLine(Sort.QuickSort(strResult));
             }
             else
             {
-                Array.Reverse(mass);
-                return new string(mass) + str;
+                Console.WriteLine("\nРезультат сортировки TreeSort: ");
+                Console.WriteLine(Sort.TreeSort(strResult));
             }
-        }
-        static bool CheckString(string str)
-        {
-            if (str.Length == 0)
+            static string ChangedStr(string str)
             {
-                Console.WriteLine("Не введены символы");
-                return false;
-            }
-            if (!Regex.IsMatch(str, "^[a-z]*$"))
-            {
-                Console.WriteLine("Ошибка ввода\nНеверные символы:");
-                Regex r = new Regex("[a-z]");
                 char[] mass = str.ToCharArray();
-                char[] result = mass.Distinct().ToArray(); //удаление дубликатов неверных символов
-                for (int i = 0; i < result.Length; i++)
+                if (str.Length % 2 == 0)
                 {
-                    if (!r.IsMatch(result[i].ToString()) || char.IsUpper(result[i]) || char.IsDigit(result[i]))
+                    int mid = (mass.Length + 1) / 2;
+                    char[] firstHalf = mass.Take(mid).ToArray();
+                    char[] secondHalf = mass.Skip(mid).ToArray();
+                    Array.Reverse(firstHalf);
+                    Array.Reverse(secondHalf);
+                    return new string(firstHalf) + new string(secondHalf);
+                }
+                else
+                {
+                    Array.Reverse(mass);
+                    return new string(mass) + str;
+                }
+            }
+            static bool CheckString(string str)
+            {
+                if (str.Length == 0)
+                {
+                    Console.WriteLine("Не введены символы");
+                    return false;
+                }
+                if (!Regex.IsMatch(str, "^[a-z]*$"))
+                {
+                    Console.WriteLine("Ошибка ввода\nНеверные символы:");
+                    Regex r = new Regex("[a-z]");
+                    char[] mass = str.ToCharArray();
+                    char[] result = mass.Distinct().ToArray(); //удаление дубликатов неверных символов
+                    for (int i = 0; i < result.Length; i++)
                     {
-                        Console.Write(result[i]);
+                        if (!r.IsMatch(result[i].ToString()) || char.IsUpper(result[i]) || char.IsDigit(result[i]))
+                        {
+                            Console.Write(result[i]);
+                        }
+                    }
+                    return false;
+                }
+                return true;
+            }
+            static void NumOfReplays(string str)
+            {
+                Console.WriteLine("Каждый символ входил в строку: ");
+                List<char> tmp = new List<char>(str);
+                foreach (var symbol in str.ToCharArray())
+                {
+                    Predicate<char> removableSymbol = enumSymbols => enumSymbols == symbol;
+                    int numDelSymbols = tmp.RemoveAll(removableSymbol);
+                    if (numDelSymbols != 0) Console.WriteLine(symbol + " = " + numDelSymbols);
+                }
+            }
+            static void SubString(string str)
+            {
+                Regex r = new Regex("^[aeiou]*$");
+                char[] tmp = str.ToCharArray();
+                Predicate<char> indexSymbol = e => r.IsMatch(e.ToString());
+                if (Array.Exists(tmp, indexSymbol))
+                {
+                    int start = Array.FindIndex(tmp, indexSymbol);
+                    int end = Array.FindLastIndex(tmp, indexSymbol);
+                    Console.WriteLine("Самая длинная подстрока начинающаяся и заканчивающаяся на гласную:");
+                    for (int i = start; i <= end; i++)
+                    {
+                        Console.Write(tmp[i].ToString());
                     }
                 }
-                return false;
-            }
-            return true;
-        }
-        static void NumOfReplays(string str)
-        {
-            Console.WriteLine("Каждый символ входил в строку: ");
-            List<char> tmp = new List<char>(str);
-            foreach (var symbol in str.ToCharArray())
-            {
-                Predicate<char> removableSymbol = enumSymbols => enumSymbols == symbol;
-                int numDelSymbols = tmp.RemoveAll(removableSymbol);
-                if (numDelSymbols != 0) Console.WriteLine(symbol + " = " + numDelSymbols);
+                else Console.WriteLine("Подстроки начинающаяся и заканчивающаяся на гласную не существует");
             }
         }
-        static void SubString(string str)
-        {
-            Regex r = new Regex("^[aeiou]*$");
-            char[] tmp = str.ToCharArray();
-            Predicate<char> indexSymbol = e => r.IsMatch(e.ToString());
-            if (Array.Exists(tmp, indexSymbol))
-            {
-                int start = Array.FindIndex(tmp, indexSymbol);
-                int end = Array.FindLastIndex(tmp, indexSymbol);
-                Console.WriteLine("Самая длинная подстрока начинающаяся и заканчивающаяся на гласную:");
-                for (int i = start; i <= end; i++)
-                {
-                    Console.Write(tmp[i].ToString());
-                }
-            }
-            else Console.WriteLine("Подстроки начинающаяся и заканчивающаяся на гласную не существует");
-        }
-        
     }
 }
---
+++ Practice1/Sort.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal static class Sort
+    {
+        //QuickSort==========================================================
+        //метод обмена элементами массива
+        private static void Swap(ref char x, ref char y)
+        {
+            var t = x;
+            x = y;
+            y = t;
+        }
+        //метод возвращает индекс ссылочного элемента
+        private static int Partition(char[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+        private static char[] QuickSort(char[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+            var pivotIndex = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, maxIndex);
+            return array;
+        }
+        public static string QuickSort(string array)
+        {
+            char[] mass = array.ToCharArray();
+            return new string(QuickSort(mass, 0, mass.Length - 1));
+        }
+        //TreeSort===========================================================
+        public static char[] TreeSort(string array)
+        {
+            var treeNode = new TreeNode(array[0]);
+            for (int i = 1; i < array.Length; i++)
+            {
+                treeNode.Insert(new TreeNode(array[i]));
+            }
+
+            return treeNode.Transform();
+        }
+    }
+    public class TreeNode
+    {
+        public TreeNode(char data)
+        {
+            Data = data;
+        }
+        //данные
+        public char Data { get; set; }
+        //левая ветка дерева
+        public TreeNode Left { get; set; }
+        //правая ветка дерева
+        public TreeNode Right { get; set; }
+        //рекурсивное добавление узла в дерево
+        public void Insert(TreeNode node)
+        {
+            if (node.Data < Data)
+            {
+                if (Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Insert(node);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Insert(node);
+                }
+            }
+        }
+        //преобразование дерева в отсортированный массив
+        public char[] Transform(List<char> elements = null)
+        {
+            if (elements == null)
+            {
+                elements = new List<char>();
+            }
+            if (Left != null)
+            {
+                Left.Transform(elements);
+            }
+            elements.Add(Data);
+            if (Right != null)
+            {
+                Right.Transform(elements);
+            }
+            return elements.ToArray();
+        }
+    }
+    //===================================================================
+}
